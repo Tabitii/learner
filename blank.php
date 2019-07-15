2112f9ac619a798f307db4dc6f2ef8ad382f86cb
Varvara <varvara-feng@mail.ru>
2019-07-16 00:06:25 +0300

    clear all app. db, php to upload

--- blank.php
+++ blank.php
@@ -1,3 +1,13 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,96 +35,95 @@
   <!-- Page Wrapper -->
   <div id="wrapper">
 
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Learner <sup>1</sup></div>
+      </a>
 
-          <!-- Sidebar - Brand -->
-          <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div class="sidebar-brand-icon rotate-n-15">
-              <i class="fas fa-laugh-wink"></i>
-            </div>
-            <div class="sidebar-brand-text mx-3">Learner <sup>1</sup></div>
-          </a>
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
 
-          <!-- Divider -->
-          <hr class="sidebar-divider my-0">
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
 
-          <!-- Nav Item - Dashboard -->
-          <li class="nav-item active">
-            <a class="nav-link" href="index.html">
-              <i class="fas fa-fw fa-tachometer-alt"></i>
-              <span>Dashboard</span></a>
-          </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider">
 
-          <!-- Divider -->
-          <hr class="sidebar-divider">
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Plan my subjects
+      </div>
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="add_subject.html">
+          <i class="fa fa-plus"></i>
+          <span>Add new subject in plan</span></a>
+      </li>
 
-          <!-- Heading -->
-          <div class="sidebar-heading">
-            Plan my subjects
-          </div>
-          <!-- Nav Item - Charts -->
-          <li class="nav-item">
-            <a class="nav-link" href="add_subject.html">
-              <i class="fa fa-plus"></i>
-              <span>Add new subject in plan</span></a>
-          </li>
-
-          <!-- Nav Item - Charts -->
-          <li class="nav-item">
-            <a class="nav-link" href="view_subjects.html">
-              <i class="fa fa-list-ol"></i>
-              <span>View all subjects</span></a>
-          </li>
-
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="view_subjects.html">
+          <i class="fa fa-list-ol"></i>
+          <span>View all subjects</span></a>
+      </li>
 
 
-          <!-- Divider -->
-          <hr class="sidebar-divider">
-          <!-- Heading -->
-          <div class="sidebar-heading">
-            Do my Plan
-          </div>
 
-          <!-- Nav Item - Charts -->
-          <li class="nav-item">
-            <a class="nav-link" href="links.html">
-              <i class="fa fa-link"></i>
-              <span>Links to do subjects</span></a>
-          </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Do my Plan
+      </div>
 
-          <!-- Nav Item - Charts -->
-          <li class="nav-item">
-            <a class="nav-link" href="timer.html">
-              <i class="fa fa-play"></i>
-              <span>Timer to do subjects</span></a>
-          </li>
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="links.html">
+          <i class="fa fa-link"></i>
+          <span>Links to do subjects</span></a>
+      </li>
+
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="timer.html">
+          <i class="fa fa-play"></i>
+          <span>Timer to do subjects</span></a>
+      </li>
 
 
-          <!-- Divider -->
-          <hr class="sidebar-divider">
-          <!-- Heading -->
-          <div class="sidebar-heading">
-            I'he already done
-          </div>
-          <!-- Nav Item - Tables -->
-          <li class="nav-item">
-            <a class="nav-link" href="learner_list.html">
-              <i class="fa fa-book"></i>
-              <span>Learner's List</span></a>
-          </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        I'he already done
+      </div>
+      <!-- Nav Item - Tables -->
+      <li class="nav-item">
+        <a class="nav-link" href="learner_list.html">
+          <i class="fa fa-book"></i>
+          <span>Learner's List</span></a>
+      </li>
 
-          <!-- Divider -->
-          <hr class="sidebar-divider d-none d-md-block">
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
 
-          <!-- Sidebar Toggler (Sidebar) -->
-          <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-          </div>
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
 
-        </ul>
-        <!-- End of Sidebar -->
+    </ul>
+    <!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
@@ -164,25 +173,17 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Yaromir</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo htmlspecialchars($_SESSION["username"]); ?></span>
                 <img class="img-profile rounded-circle" src="https://pp.userapi.com/c857720/v857720834/e0f9/_K46eCJ8b6o.jpg">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="profile.html">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="settings.html">
+                <a class="dropdown-item" href="reset-password.php">
                   <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
-                </a>
-                <a class="dropdown-item" href="activity.html">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
+                  Reset your password
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
